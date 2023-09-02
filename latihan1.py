6d71ac4e47f4e0898d1c5cd9c801c121fedeeb47
veldoraaa <143859751+Veldoraaa-sama@users.noreply.github.com>
2023-09-01 20:59:30 -0700

    Add files via upload
    latihan python 1

---
+++ latihan1.py
@@ -0,0 +1,11 @@
+nama_saya = str(input("Vel = " ))
+print(nama_saya, "bertipe", type(nama_saya))
+
+umur = int(input("18 = "))
+print(umur, "bertipe", type(umur))
+
+IPK = float(input("3.00 = "))
+print(IPK, "bertipe", type(IPK))
+
+menikah = bool(input("Belum = "))
+print("menikah?",menikah, "bertipe", type(menikah))
